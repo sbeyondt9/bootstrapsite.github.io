1b29e6d7b8d3fa2a8331f28b541b0bcf4a65c158
sbeyondt9 <73100240+sbeyondt9@users.noreply.github.com>
2020-12-09 12:48:20 +0530

    d
    d

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,94 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["subject"];
-	$content = $_POST["content"];
+//index.php
 
-	$conn = mysqli_connect("localhost", "root", "test", "blog_samples") or die("Connection Error: " . mysqli_error($conn));
-	mysqli_query($conn, "INSERT INTO tblcontact (user_name, user_email,subject,content) VALUES ('" . $name. "', '" . $email. "','" . $subject. "','" . $content. "')");
-	$insert_id = mysqli_insert_id($conn);
-	//if(!empty($insert_id)) {
-	   $message = "Your contact information is saved successfully.";
-	   $type = "success";
-	//}
+$error = '';
+$name = '';
+$email = '';
+$subject = '';
+$message = '';
+
+function clean_text($string)
+{
+ $string = trim($string);
+ $string = stripslashes($string);
+ $string = htmlspecialchars($string);
+ return $string;
 }
-require_once "contact-view.php";
 
-
-
-
-
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["subject"];
-	$content = $_POST["content"];
-
-	$toEmail = "santosh@beyondt.in";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
+if(isset($_POST["submit"]))
+{
+ if(empty($_POST["name"]))
+ {
+  $error .= '<p><label class="text-danger">Please Enter your Name</label></p>';
+ }
+ else
+ {
+  $name = clean_text($_POST["name"]);
+  if(!preg_match("/^[a-zA-Z ]*$/",$name))
+  {
+   $error .= '<p><label class="text-danger">Only letters and white space allowed</label></p>';
+  }
+ }
+ if(empty($_POST["email"]))
+ {
+  $error .= '<p><label class="text-danger">Please Enter your Email</label></p>';
+ }
+ else
+ {
+  $email = clean_text($_POST["email"]);
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+  {
+   $error .= '<p><label class="text-danger">Invalid email format</label></p>';
+  }
+ }
+ if(empty($_POST["subject"]))
+ {
+  $error .= '<p><label class="text-danger">Subject is required</label></p>';
+ }
+ else
+ {
+  $subject = clean_text($_POST["subject"]);
+ }
+ if(empty($_POST["message"]))
+ {
+  $error .= '<p><label class="text-danger">Message is required</label></p>';
+ }
+ else
+ {
+  $message = clean_text($_POST["message"]);
+ }
+ if($error == '')
+ {
+  require 'class/class.phpmailer.php';
+  $mail = new PHPMailer;
+  $mail->IsSMTP();        //Sets Mailer to send message using SMTP
+  $mail->Host = 'smtpout.secureserver.net';  //Sets the SMTP hosts
+  $mail->Port = '80';        //Sets the default SMTP server port
+  $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
+  $mail->Username = 'xxxxxxxxxx';     //Sets SMTP username
+  $mail->Password = 'xxxxxxxxxx';     //Sets SMTP password
+  $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
+  $mail->From = $_POST["email"];     //Sets the From email address for the message
+  $mail->FromName = $_POST["name"];    //Sets the From name of the message
+  $mail->AddAddress('info@find2rent.com', 'Name');//Adds a "To" address
+  $mail->AddCC($_POST["email"], $_POST["name"]); //Adds a "Cc" address
+  $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
+  $mail->IsHTML(true);       //Sets message type to HTML    
+  $mail->Subject = $_POST["subject"];    //Sets the Subject of the message
+  $mail->Body = $_POST["message"];    //An HTML or plain text message body
+  if($mail->Send())        //Send an Email. Return true on success or false on error
+  {
+   $error = '<label class="text-success">Thank you for contacting us</label>';
+  }
+  else
+  {
+   $error = '<label class="text-danger">There is an Error</label>';
+  }
+  $name = '';
+  $email = '';
+  $subject = '';
+  $message = '';
+ }
 }
-require_once "contact-view.php";
+
 ?>
