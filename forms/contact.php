fbabfa23d8804ff8e37abda98c914a5cc1418d16
sbeyondt9 <73100240+sbeyondt9@users.noreply.github.com>
2020-12-09 13:36:58 +0530

    f
    f

--- forms/contact.php
+++ forms/contact.php
@@ -21,6 +21,6 @@ $validator->field('Message')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('someone@gmail.com'); // ← Your email here
+$pp->sendEmailTo('earncoin1105@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
