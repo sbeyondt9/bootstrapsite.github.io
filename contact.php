0b55277acf966341975f8074a0a685a402f8f2f6
million-dollar-baby <93.abhigupta@gmail.com>
2021-03-12 18:16:43 +0530

    b
    b

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "bobbyvatsa@yahoo.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
+$headers = "From: noreply@kreativekarma.in" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
